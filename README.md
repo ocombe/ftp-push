031dcfec2bb9e6be3bbcdea64b3b16f270f39a04
ocombe <olivier.combe@gmail.com>
2013-08-04 20:24:11 +0200

    version 0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
 ftp-push
 ========
 
-CLI program to push local files &amp; folders to a remote FTP
+A CLI program to push local files &amp; folders to a remote FTP
+
+# Installation & usage :
+1. Download and install [nodeJS](http://nodejs.org/download/).
+
+
+2. Go to the ftp-push folder and launch npm to download and compile the modules for your environment :
+
+	```npm install```
+
+
+3. Use the program with a command line :
+
+
+	```node .app.js -pr -h myhost.com -u MyUserName -w MyPasswd -s "C:\localSourceFileOrFolder" -d "/home/RemoteFolder"```
+
+	Options:
+	```
+	  -h, --help                  output usage information
+	  -V, --version               output the version number
+	  -h, --host [host]           Host
+	  -o, --port [port]           Port
+	  -u, --user [user]           User
+	  -w, --password [password]   Password
+	  -p, --put                   Put
+	  -s, --source [file]         The source file/folder
+	  -d, --destination [folder]  The destination folder
+	  -r, --recursive             Recursive
+	  -c, --clean                 Delete local file/folder after transfer
+	  ```
---
+++ app.js
@@ -0,0 +1,93 @@
+#!/usr/bin/env node
+
+var program = require('commander');
+
+program
+	.version('0.0.1')
+	.usage('node .\app.js -pr -h myhost.com -u MyUserName -w MyPasswd -s "C:\localSourceFileOrFolder" -d "/home/RemoteFolder"')
+	.option('-h, --host [host]', 'Host')
+	.option('-o, --port [port]', 'Port', 21)
+	.option('-u, --user [user]', 'User')
+	.option('-w, --password [password]', 'Password')
+	.option('-p, --put', 'Put')
+	.option('-s, --source [file]', 'The source file/folder')
+	.option('-d, --destination [folder]', 'The destination folder')
+	.option('-r, --recursive', 'Recursive')
+	.option('-c, --clean', 'Delete local file/folder after transfer')
+	.parse(process.argv);
+
+if(program.host && program.user && program.password && program.put && program.source && program.destination) {
+	var FTPClient = require('ftp'),
+		wrench = require('wrench'),
+		fs = require('fs'),
+		path = require('path'),
+		async = require('async');
+
+	var pJoin = function(a, b) {
+		return path.join(a, b).replace(/\\/g, '/');
+	}
+
+	var ftp = new FTPClient();
+	ftp.on('ready', function() {
+		fs.stat(program.source, function(err, stats) {
+			if(err) {
+				throw err;
+			} else if(!err && stats.isDirectory()) {
+				if(program.recursive) {
+					async.mapSeries(wrench.readdirSyncRecursive(program.source), function(file, callback) {
+						fs.stat(pJoin(program.source, file), function(err, stats) {
+							if(err) {
+								throw err;
+							} else if(!err && stats.isDirectory()) {
+								ftp.mkdir(pJoin(program.destination, file), true, callback);
+							} else {
+								ftp.put(pJoin(program.source, file), pJoin(program.destination, file), callback);
+							}
+						});
+					}, function(err, res) {
+						if(err) {
+							throw err;
+						}
+						console.log("Folder transferred successfully!");
+						if(program.clean) {
+							wrench.rmdirSyncRecursive(program.source);
+							console.log('Local folder deleted successfully');
+						}
+						ftp.end();
+					});
+				} else {
+					ftp.mkdir(pJoin(program.destination, path.basename(program.source)), true, function(err) {
+						if(err) {
+							throw err;
+						}
+						console.log("Folder transferred successfully!");
+						if(program.clean) {
+							wrench.rmdirSyncRecursive(program.source);
+							console.log('Local folder deleted successfully');
+						}
+						ftp.end();
+					});
+				}
+			} else {
+				ftp[program.put ? 'put' : 'get'](program.source, pJoin(program.destination, path.basename(program.source)), function(err) {
+					if(err) {
+						throw err;
+					}
+					console.log("File transferred successfully!");
+					if(program.clean) {
+						fs.unlinkSync(program.source);
+						console.log('Local file deleted successfully');
+					}
+					ftp.end();
+				});
+			}
+		});
+	});
+
+	ftp.connect({
+		host: program.host,
+		port: program.port, // defaults to 21
+		user: program.user,
+		password: program.password
+	});
+}
